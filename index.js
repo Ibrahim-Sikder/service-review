f067d7992e90cf528164627ffc65eeb4f863f031
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-11-10 19:52:34 +0600

    review-delete-api-create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -11,13 +11,27 @@ const port = process.env.PORT || 5000 ;
 app.use(cors())
 app.use(express.json())
 
-// Database user
-// user : Ibrahim-Sikder1
-// password : 35VaCyZxmBQ2uaiq
 // Database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.u82gun1.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// jwt verify token
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        res.status(401).send({message: 'Unauthorized access '})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            res.status(403).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded ;
+        next()
+    })
+ }
+
  
 async function run(){
 try{
@@ -26,6 +40,13 @@ try{
     const addServiceCollection = client.db('Food-Service').collection('addServices')
 
 
+    // JWT api 
+    app.post('/jwt', (req, res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRETE, {expiresIn: '1h'})
+        console.log(user)
+        res.send({token})
+    })
     
    // add service api 
    app.post('/addServices', async (req, res)=>{
@@ -55,7 +76,15 @@ try{
     const review = await cursor.toArray();
     res.send(review)
    })
+    // review delete api 
+    app.delete('/review/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await reviewCollection.deleteOne(query);
+        res.send(result);
+    })
 
+  
 
 
    // for home page service find  query
@@ -65,6 +94,7 @@ try{
         const services  = await cursor.limit(3).toArray();
         res.send(services);
     })
+    // see all service 
     app.get('/allService', async(req, res)=>{
         const query = {} 
         const cursor = serviceCollection.find(query);
@@ -72,14 +102,15 @@ try{
         res.send(services);
     })
 
+   // service details api 
+
     app.get('/services/:id', async (req, res)=>{
         const id = req.params.id;
         const query = {_id: ObjectId(id)};
         const service = await serviceCollection.findOne(query) ;
         res.send(service)
     })
-
-  
+   
    
 
 }
@@ -91,12 +122,12 @@ run().catch(err=>console.log(err))
 
 
 
-// Default server running
+// default api 
 
 app.get('/', (req, res)=>{
     res.send('Food service project running !!')
 })
 
 app.listen(port, ()=>{
-    console.log(`Food service project kaj kortece tik moto ! ${port}`)
+    console.log(`I am running finally ! ${port}`)
 })
